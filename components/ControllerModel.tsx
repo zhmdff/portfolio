fe53d87e88a1d93a778cd699be5537cb999c8cc0
zhmdff <mahmud.ehmed06@gmail.com>
2026-08-05 20:48:53 +0400

    perf: cap canvas dpr and drop HDRI environment on controller model

--- components/ControllerModel.tsx
+++ components/ControllerModel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { useGLTF, Environment, Float, Center } from "@react-three/drei";
+import { useGLTF, Float, Center } from "@react-three/drei";
 import { Suspense } from "react";
 import { useTheme } from "@/context/ThemeContext";
 
@@ -29,13 +29,17 @@ export default function ControllerModel() {
 
   return (
     <div className="controller-canvas-wrapper">
-      <Canvas camera={{ position: [0, 0, 12], fov: 40 }} style={{ width: "100%", height: "100%" }} gl={{ antialias: true, alpha: true }}>
+      <Canvas
+        camera={{ position: [0, 0, 12], fov: 40 }}
+        dpr={[1, 1.5]}
+        style={{ width: "100%", height: "100%" }}
+        gl={{ antialias: true, alpha: true }}
+      >
         <Suspense fallback={null}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[5, 5, 5]} intensity={1} />
           <directionalLight position={[-3, 2, -2]} intensity={0.4} color="#a78bfa" />
           <Controller key={modelPath} modelPath={modelPath} />
-          <Environment preset="city" />
         </Suspense>
       </Canvas>
     </div>
