ebe4c44bcd403ae7cf5505c9b35e05187f02bc43
zhmdff <mahmud.ehmed06@gmail.com>
2026-08-05 21:14:29 +0400

    perf: enable optimizePackageImports for three/drei

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: ["three", "@react-three/drei"],
+  },
 };
 
 export default nextConfig;
